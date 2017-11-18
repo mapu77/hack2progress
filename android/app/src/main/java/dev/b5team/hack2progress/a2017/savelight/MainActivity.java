58725dc7f1b0817235f8ff63c971ae24c0a9c065
Marc Vila <marcvilagomez@gmail.com>
2017-11-18 15:51:58 +0100

    Notifications Android working

--- android/app/src/main/java/dev/b5team/hack2progress/a2017/savelight/MainActivity.java
+++ android/app/src/main/java/dev/b5team/hack2progress/a2017/savelight/MainActivity.java
@@ -4,19 +4,83 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import dev.b5team.hack2progress.a2017.savelight.controllers.VolleyController;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private static final String NOTIFICATIONS_ENDPOINT = "https://efi-home-sergiowalls.c9users.io:8080/devices";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        final String token = getToken();
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("deviceToken", token);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        final String requestBody = jsonBody.toString();
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, NOTIFICATIONS_ENDPOINT,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.getMessage());
+                    }
+                }
+        ) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    return null;
+                }
+            }
+        };
+        VolleyController.getInstance(this).addToQueue(postRequest);
+
         Fragment fragment = HomeFragment.newInstance();
         String fragmentTAG = HomeFragment.TAG;
-
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.content, fragment, fragmentTAG);
         ft.commit();
+    }
+
+    private String getToken() {
+        String token = FirebaseInstanceId.getInstance().getToken();
+//        Log.e("Firebase Token: ", token);
+        return token;
     }
 }
